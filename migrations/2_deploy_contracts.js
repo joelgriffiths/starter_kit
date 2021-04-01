d38b8c9414478e9f7ed0edf0be3e097f3a6c2df9
Joel Griffiths <joelg@gadgetwiz.com>
2021-03-31 22:18:44 -0500

    Working with first mocha test

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,16 @@
+const Token = artifacts.require("Token");
+const EthSwap = artifacts.require("EthSwap");
+
+module.exports = async function(deployer) {
+    // Deploy Token
+    await deployer.deploy(Token);
+    const token = await Token.deployed()
+
+    // Deploy EthSwap
+    await deployer.deploy(EthSwap);
+    const ethSwap = await EthSwap.deployed()
+
+    // Transfer all the tokens to the EthSwap Exchange
+    await token.transfer(ethSwap.address, '1000000000000000000000000')
+
+};
---
+++ test/EthSwap.test.js
@@ -0,0 +1,18 @@
+
+const Token = artifacts.require('Token')
+const EthSwap = artifacts.require('EthSwap')
+
+// Configures assertion?
+require('chai')
+  .use(require('chai-as-promised'))
+  .should()
+
+contract('EthSwap', (accounts) => {
+    describe('EthSwap deployment', async () => {
+        it('contract has a name', async () => {
+            let ethSwap = await EthSwap.new()    
+            const name = await ethSwap.name()
+            assert.equal(name, "EthSwap Instant Exchange")
+        })
+    })
+})
